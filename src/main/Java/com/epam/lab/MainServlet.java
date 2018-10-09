17eba6bf132a7feabd89c9e0db07a3b6fab131f8
0305zhanna <0305zhanna@email.ru>
2018-10-09 21:10:09 +0300

    5 commit

--- src/main/Java/com/epam/lab/MainServlet.java
+++ src/main/Java/com/epam/lab/MainServlet.java
@@ -8,12 +8,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class MainServlet extends HttpServlet {
     private List<String> states = new ArrayList<>();
-    private AtomicInteger amount = new AtomicInteger(0);
-//    private int amount = 0;
+    private int amount = 0;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -34,21 +32,18 @@ public class MainServlet extends HttpServlet {
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         states.add("PUT");
-//        req.setAttribute("states", states);
-//        req.getRequestDispatcher("WEB-INF/jsp/index.jsp").forward(req, resp);
-    }
+        resp.setHeader("Content-type", "text/html");
+        resp.getWriter().write("PUT");    }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        states.add("DELETE");
         states.clear();
-//        req.setAttribute("states", states);
-//        req.getRequestDispatcher("WEB-INF/jsp/index.jsp").forward(req, resp);
+        resp.setHeader("Content-type", "text/html");
+        resp.getWriter().write("DELETE");
     }
 
     private void addCookie(HttpServletRequest req, HttpServletResponse resp){
-        Cookie cookie = new Cookie("amount", Integer.toString(amount.incrementAndGet()));
-//        Cookie cookie = new Cookie("amount", Integer.toString(++amount));
+        Cookie cookie = new Cookie("amount", Integer.toString(++amount));
         resp.addCookie(cookie);
 
         Cookie[] cookies = req.getCookies();
